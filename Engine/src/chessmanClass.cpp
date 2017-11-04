c7844485be06a5a342f0a68647b789e4bfeb3117
Fred <MisfitFred@users.noreply.github.com>
2017-11-04 12:34:29 +0100

    - Complete first interface definition

---
+++ Engine/src/chessmanClass.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "chessmanClass.h"
+/*
+constexpr const unsigned int operator!=(chessmanClass a, unsigned int b)
+{
+	return static_cast<unsigned int>(a) != b;
+}
+
+constexpr unsigned int operator==(chessmanClass a, unsigned int b)
+{
+	return static_cast<unsigned int>(a) == b;
+}
+
+*/
